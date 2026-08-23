8027b62d12c714ef9faeb219174e9971f1351d40
MaoZedongGaming <somekindofgowpepege@gmail.com>
2026-08-24 02:11:19 +0800

    p_2 is actually the right coordinate?

--- KerrRayTracer/relativistic_camera.cpp
+++ KerrRayTracer/relativistic_camera.cpp
@@ -91,14 +91,14 @@ void RelativisticCamera::generatePhotons() {
 		double screenY = (1.0 - 2.0 * y / (double)height) * tan(fov / 2.0);
 
 		// momenta in the camera's tetrad frame, p_t = E = 1.0
-		double p_1 = -1.0 / sqrt(1.0 + screenX * screenX + screenY * screenY); // x is the forward coordinate in this instance
-		double p_2 = screenY * p_1;
+		double p_1 = 1.0 / sqrt(1.0 + screenX * screenX + screenY * screenY); // x is the forward coordinate in this instance
+		double p_2 = screenY * p_1; // y is actually the right coordinate?
 		double p_3 = screenX * p_1;
 
 		// \eta^{\mu \nu} p_\mu = -(1.0)^2 + |p_i|^2 =  -1.0 + 1.0 = 0 so it's a proper lightlike 4 vector
 
 		// momentum projected onto global coordinates
-		Vector4d p = frame.e0 + p_1 * frame.e1 + p_2 * frame.e2 + p_3 * frame.e3;
+		Vector4d p = frame.e0 + -p_1 * frame.e1 + -p_2 * frame.e2 + p_3 * frame.e3;
 
 		// conserved covariant constants
 		double E = -(p[0] * g_tt(r, theta) + p[3] * g_tphi(r, theta)); // -p_t
